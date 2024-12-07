4f6b07a69f22e3f9f09ea15965f1bc682dd08c35
Emmanuel Princewill <alyaebrar39@gmail.com>
2024-12-08 00:09:15 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// if Statement
+
+let apple = 50;
+let grape = 50;
+
+if(apple == grape){
+    console.log("It's Working!");
+}
+
+if(apple <= grape){
+    console.log("true!");
+}
+
+
+let coconut = 30;
+let waterlemon = "40";
+
+if(coconut === waterlemon){
+    console.log("false!");
+}
+
+// else statement
+
+let mango = true;
+
+if(mango){
+    console.log("It's true!")
+}else{
+    console.lo("It's false!")
+}
+
+
+if(apple >= grape){
+    console.log("True Words")
+}else{
+    console.log("False Words")
+}
+
+
+
+if(apple){
+console.log("boolean rules!")
+}else{
+    console.log("boolean are lame!")
+}
